49b9a705f041aa769e8e297c53a4041e174ea598
Devashish jindal <60930823+drace28@users.noreply.github.com>
2023-06-14 20:20:03 +0530

    Update cal.java

--- cal.java
+++ cal.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class cal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float x, y;
+        float a, b;
         char operator;
         System.out.print("Enter first number: ");
         a = scanner.nextFloat();
